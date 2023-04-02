3fa49c7c9beba797ab5b651d1fd546db48020801
rodfer0x80 <rodfer0x80@proton.me>
2023-04-02 02:21:43 +0100

    time done; missing date; missing calendar api

--- script.js
+++ script.js
@@ -11,8 +11,7 @@ var grammar = "#JSGF V1.0; grammar calendar; public <calendar> =  ...;";
 //Regular expressions for the different utternaces
 var u1 = /.*book(| a| an) even(t|ts)$/;
 var u2 = /.*book(| a| an) even(t|ts) from ((1[0-2]|0?[1-9])|(1[0-2]|0?[1-9]):([0-5]?[0-9]))(| a\.m\.| p\.m\.)*$/i;
-var u3 = /.*book(| a| an) even(t|ts) from ((1[0-2]|0?[1-9])|(1[0-2]|0?[1-9]):([0-5]?[0-9]))(| a\.m\.| p\.m\.) to (1[0-2]|0?[1-9]):([0-5]?[0-9])(| a\.m\.| p\.m\.)$/i;
-
+var u3 = /.*book(| a| an) even(t|ts) from ((1[0-2]|0?[1-9])|(1[0-2]|0?[1-9]):([0-5]?[0-9]))(| a\.m\.| p\.m\.) to ((1[0-2]|0?[1-9])|(1[0-2]|0?[1-9]):([0-5]?[0-9]))(| a\.m\.| p\.m\.)*$/i 
 var u4 = /(?:from)?\s*((1[0-2]|0?[1-9])|(1[0-2]|0?[1-9]):([0-5]?[0-9]))(| a\.m\.| p\.m\.)$/i;
 var u5 = /(?:to)?\s*((1[0-2]|0?[1-9])|(1[0-2]|0?[1-9]):([0-5]?[0-9]))(| a\.m\.| p\.m\.)$/i;
 
@@ -110,8 +109,8 @@ recognition.onresult = function (event) {
   switch (state) {
     case 0:
       if (text.match(u1)) { enterState(1); }
-      else if (m = text.match(u3)) { startPlace = m[1]; endPlace = m[2]; enterState(5); } // "...from .... to ..."
-      else if (m = text.match(u2)) { startPlace = m[1]; enterState(3); } // "...from ...."
+      else if (m = text.match(u3)) { if (m[12]==' '){ret=m[3]+" "+"p.m."}else{ret=m[3]+" "+m[7]};startPlace=ret;  if (m[12==' ']){ret2=m[8]+" "+"p.m."} else{ ret2=m[8]+" "+m[12]};endPlace=ret2; console.log(m); enterState(5);} // "...from .... to ..."
+      else if (m = text.match(u2)) { if (m[12]==' '){ret=m[3]+" "+"p.m."}else{ret=m[3]+" "+m[7]};startPlace=ret; console.log(m);enterState(3); } // "...from ...."
       else {
         // re-enter the current state.
         // would be better to give an error message here as well:
@@ -120,7 +119,10 @@ recognition.onresult = function (event) {
       break;
     case 1:
       if (m = text.match(u4)) { // "...from ...."
-        console.log("Matched - pickup"); startPlace = m[1]+" "+m[5]; enterState(3);console.log(m);
+        var ret = "";
+        if (m[5] == '') {ret = m[1]+" "+"p.m.";}
+        else {ret = m[1]+" "+m[5];}
+        console.log("Matched - pickup"); startPlace = ret;console.log(m); enterState(3);;
       } else {
         // re-enter the current state.
         // would be better to give an error message here as well:
@@ -130,8 +132,10 @@ recognition.onresult = function (event) {
 
     case 3:
       if (m = text.match(u5)) {
+        if (m[5] == '') {ret = m[1]+" "+"p.m.";}
+        else {ret = m[1]+" "+m[5];}
         console.log("Matched - destination");
-        endPlace = m[1]+" "+m[5]; enterState(5);;
+        endPlace = ret; enterState(5);console.log(m);
       } else {
         // re-enter the current state.
         // would be better to give an error message here as well:
